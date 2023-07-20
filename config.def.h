cd1f9ba67bb7d938438ba4af20f308124a87536e
delete <delete@thinkarch.localdomain>
2023-07-20 23:29:19 +0300

    final dwm config

--- config.def.h
+++ config.def.h
@@ -10,13 +10,18 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+#define MAIN_FONT "JetBrains Mono Medium:size=10"
+static const char *fonts[]          = { 
+	MAIN_FONT,
+	"Font Awesome 6 Free Solid:size=10"
+};
+static const char dmenufont[]       = MAIN_FONT;
+static const char col_gray1[]       = "#004E98"; //background
+static const char col_gray2[]       = "#3A6EA5"; //border not sel
+static const char col_gray3[]       = "#C0C0C0";
+static const char col_gray4[]       = "#EBEBEB";
+static const char col_cyan[]        = "#FF6700"; //border and osn col
+static const char col_hilgh[]       = "#4E7390";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -33,6 +38,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Arandr",   NULL,       NULL,       0,            1,           -1 },
+	{ "Blueman-manager",NULL, NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -45,8 +52,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -62,12 +69,66 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dwm-dmenu-all", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dwm-dmenu-all", "-m", dmenumon, 
+        "-fn", dmenufont,   //font
+        "-nb", col_gray1,   // not selected back color
+        "-nf", col_gray3,   // not selected font color
+        "-sb", col_cyan,    // selected     back color
+        "-sf", col_gray4,   // selected     font color
+                            // FZF highlight
+        "-nhb", col_gray1,  // not selected back color
+        "-nhf", col_hilgh,  // not selected font color
+        "-shb", col_cyan,   // selected     back color
+        "-shf", col_hilgh,  // selected     font color
+        NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+static const char *mute_volume_cmd   []    = { "pactl",   "set-sink-mute",   "@DEFAULT_SINK@",        "toggle",   NULL };
+static const char *down_volume_cmd   []    = { "pactl",   "set-sink-volume", "@DEFAULT_SINK@",        "-5%",      NULL };
+static const char *up_volume_cmd     []    = { "pactl",   "set-sink-volume", "@DEFAULT_SINK@",        "+5%",      NULL };
+static const char *mute_mic_cmd      []    = { "pactl",   "set-source-mute", "@DEFAULT_SOURCE@",      "toggle",   NULL };
+
+static const char *down_backlight_cmd[]    = { "brightnessctl",   "set",    "5-%",   NULL };
+static const char *up_backlight_cmd  []    = { "brightnessctl",   "set",    "5+%",   NULL };
+
+
+static const char *print_dunst_volume_cmd  []    = { "print_dunst_volume",   NULL };
+static const char *flameshot_cmd[]    = { "flameshot", "gui",   NULL };
+//      XF86AudioMute,         
+//      XF86AudioLowerVolume,  
+//      XF86AudioRaiseVolume,  
+//      XF86AudioMicMute,      
+//                               
+//      XF86MonBrightnessDown, 
+//      XF86MonBrightnessUp,   
+//      XF86Display,           
+//      XF86WLAN,              
+//                               
+//      XF86Messenger,         
+//      XF86Go,                
+//      Cancel,                
+//      XF86Favorites,         
+//                               
+//      Print,                 
+void
+volume(const Arg *arg) {
+	Arg a = {.v = print_dunst_volume_cmd};
+    spawn(arg);
+    spawn(&a);
+}
 #include "movestack.c"
+#include <X11/XF86keysym.h>
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{      0,    XF86XK_AudioMute,             volume,         {.v = mute_volume_cmd} },
+	{      0,    XF86XK_AudioLowerVolume,      volume,         {.v = down_volume_cmd} },
+	{      0,    XF86XK_AudioRaiseVolume,      volume,         {.v = up_volume_cmd} },
+	{      0,    XF86XK_AudioMicMute,          spawn,          {.v = mute_mic_cmd       } },
+                                                                                
+	{      0,    XF86XK_MonBrightnessDown,     spawn,          {.v = down_backlight_cmd } },
+	{      0,    XF86XK_MonBrightnessUp,       spawn,          {.v = up_backlight_cmd   } },
+
+	{      0,    XK_Print,                     spawn,          {.v = flameshot_cmd   } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 //	{ MODKEY,                       XK_b,      togglebar,      {0} },
