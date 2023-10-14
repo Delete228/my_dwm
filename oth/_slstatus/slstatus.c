30a78d98b2f1cdb05e35c193a4cca6cf26f8ff96
delete <delete@thinkarch.localdomain>
2023-10-15 02:25:09 +0300

    add dmenu and slstatus

---
+++ oth/_slstatus/slstatus.c
@@ -0,0 +1,169 @@
+/* See LICENSE file for copyright and license details. */
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <X11/Xlib.h>
+
+#include "arg.h"
+#include "slstatus.h"
+#include "util.h"
+
+struct arg {
+	const char *(*func)(const char *);
+	const char *fmt;
+	const char *args;
+};
+
+char buf[1024];
+static volatile sig_atomic_t done;
+static Display *dpy;
+
+int dwlflag;
+#include "config.h"
+
+static void
+terminate(const int signo)
+{
+	if (signo != SIGUSR1)
+		done = 1;
+}
+
+static void
+difftimespec(struct timespec *res, struct timespec *a, struct timespec *b)
+{
+	res->tv_sec = a->tv_sec - b->tv_sec - (a->tv_nsec < b->tv_nsec);
+	res->tv_nsec = a->tv_nsec - b->tv_nsec +
+	               (a->tv_nsec < b->tv_nsec) * 1E9;
+}
+
+static void
+usage(void)
+{
+	die("usage: %s [-v] [-s] [-1] [-d]", argv0);
+}
+
+#include<fcntl.h>
+static char somebarPath[128];
+static int somebarFd = -1;
+
+void
+psomebar(char *statusstr)
+{
+	if (somebarFd < 0) {
+		somebarFd = open(somebarPath, O_WRONLY|O_CLOEXEC);
+		if (somebarFd < 0 && errno == ENOENT) {
+			// assume somebar is not ready yet
+			somebarFd = open(somebarPath, O_WRONLY|O_CLOEXEC);
+		}
+		if (somebarFd < 0) {
+			perror("open");
+			return;
+		}
+	}
+	dprintf(somebarFd, "status %s\n", statusstr);
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct sigaction act;
+	struct timespec start, current, diff, intspec, wait;
+	size_t i, len;
+	int sflag, ret;
+	char status[MAXLEN];
+	const char *res;
+
+	sflag = 0;
+    dwlflag = 0;
+	ARGBEGIN {
+	case 'v':
+		die("slstatus-"VERSION);
+	case '1':
+		done = 1;
+		/* FALLTHROUGH */
+	case 's':
+		sflag = 1;
+		break;
+	case 'd':
+		dwlflag = 1;
+		break;
+	default:
+		usage();
+	} ARGEND
+
+	if (argc)
+		usage();
+
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = terminate;
+	sigaction(SIGINT,  &act, NULL);
+	sigaction(SIGTERM, &act, NULL);
+	act.sa_flags |= SA_RESTART;
+	sigaction(SIGUSR1, &act, NULL);
+
+	if (!sflag && !dwlflag && !(dpy = XOpenDisplay(NULL)))
+		die("XOpenDisplay: Failed to open display");
+
+	if (!strlen(somebarPath) && dwlflag) {
+		strcpy(somebarPath, getenv("XDG_RUNTIME_DIR"));
+		strcat(somebarPath, "/dwl-bar-0");
+	}
+
+	do {
+		if (clock_gettime(CLOCK_MONOTONIC, &start) < 0)
+			die("clock_gettime:");
+
+		status[0] = '\0';
+		for (i = len = 0; i < LEN(args); i++) {
+			if (!(res = args[i].func(args[i].args)))
+				res = unknown_str;
+
+			if ((ret = esnprintf(status + len, sizeof(status) - len,
+			                     args[i].fmt, res)) < 0)
+				break;
+
+			len += ret;
+		}
+
+		if (sflag) {
+			puts(status);
+			fflush(stdout);
+			if (ferror(stdout))
+				die("puts:");
+		} else {
+            if (dwlflag) {
+                 psomebar(status);
+            } else {
+			    if (XStoreName(dpy, DefaultRootWindow(dpy), status) < 0)
+			    	die("XStoreName: Allocation failed");
+			    XFlush(dpy);
+            }
+		}       
+                
+		if (!done) {
+			if (clock_gettime(CLOCK_MONOTONIC, &current) < 0)
+				die("clock_gettime:");
+			difftimespec(&diff, &current, &start);
+
+			intspec.tv_sec = interval / 1000;
+			intspec.tv_nsec = (interval % 1000) * 1E6;
+			difftimespec(&wait, &intspec, &diff);
+
+			if (wait.tv_sec >= 0 &&
+			    nanosleep(&wait, NULL) < 0 &&
+			    errno != EINTR)
+					die("nanosleep:");
+		}
+	} while (!done);
+
+	if (!sflag) {
+		XStoreName(dpy, DefaultRootWindow(dpy), NULL);
+		if (XCloseDisplay(dpy) < 0)
+			die("XCloseDisplay: Failed to close display");
+	}
+
+	return 0;
+}
